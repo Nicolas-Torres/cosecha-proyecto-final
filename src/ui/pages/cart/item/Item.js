652ba64a9e5b8903657867b8099bb1b6fbc548bc
nk1nG <61366960+nk1nG@users.noreply.github.com>
2022-06-16 23:47:35 -0500

    Desafío 6

--- src/ui/pages/cart/item/Item.js
+++ src/ui/pages/cart/item/Item.js
@@ -6,6 +6,7 @@ const Item = ({ id, title, picUrl, price, stock }) => {
         <h1>{title}</h1>
         <img src={picUrl} alt={title} width="300" heigth="auto"/>
         <h2>Precio: {price}</h2>
+        
         <ItemCount stock={stock} initial={1}/>
     </div>
   )
